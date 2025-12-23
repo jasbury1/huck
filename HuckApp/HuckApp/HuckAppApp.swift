460ed5ae9d5a94fa16b3739b5a14cc81ec38cff8
James Asbury <jamesasbury03@gmail.com>
2025-12-22 23:04:22 -0800

    XCode project template

---
+++ HuckApp/HuckApp/HuckAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuckAppApp.swift
+//  HuckApp
+//
+//  Created by James Asbury on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HuckAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
