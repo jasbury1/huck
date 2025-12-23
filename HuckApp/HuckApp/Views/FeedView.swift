00fbbd70c488381ecafd26bafb85de53e9b61d8b
James Asbury <jamesasbury03@gmail.com>
2025-12-23 14:55:56 -0800

    Display story title

--- HuckApp/HuckApp/Views/FeedView.swift
+++ HuckApp/HuckApp/Views/FeedView.swift
@@ -17,13 +17,30 @@ struct FeedView: View {
     }
 }
 
+struct StoryCellView: View {
+    let storyId: Int
+    private let observableStory: StoryCellViewData
+    
+    init(storyId: Int) {
+        self.storyId = storyId
+        self.observableStory = StoryCellViewData(storyId: storyId)
+    }
+    
+    var body: some View {
+        Text(observableStory.story?.title ?? "Title")
+        .task {
+            await observableStory.getStory()
+        }
+    }
+}
+
 struct StoriesView: View {
     let observableStories = StoriesViewObject()
     
     var body: some View {
         List {
             ForEach(observableStories.storyIds, id: \.self) {item in
-                Text("\(item)")
+                StoryCellView(storyId: item)
             }
         }
         .listStyle(.plain)
@@ -43,9 +60,39 @@ class StoriesViewObject {
     }
 }
 
-func getStoryIdsAsync() async -> [Int]{
+@Observable
+class StoryCellViewData {
+    let storyId: Int
+    var story: Story? = nil
+    
+    init(storyId: Int) {
+        self.storyId = storyId
+    }
+    
+    func getStory() async {
+        self.story = await getStoryAsync(id: self.storyId)
+    }
+}
+
+// TODO: eventually needs an enum for if we are sorting by top, best, or new
+func getStoryIdsAsync() async -> [Int] {
     let url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-    guard let stories: [Int] = await WebService().downloadData(fromURL: url) else {return []}
+    guard let stories: [Int] = await WebService().downloadData(fromURL: url) else {
+        return []
+    }
     print("Stories: \(stories)")
     return stories
+}
+
+struct Story: Codable {
+    let title: String
+}
+
+func getStoryAsync(id: Int) async -> Story? {
+    let url = "https://hacker-news.firebaseio.com/v0/item/\(id).json?print=pretty"
+    guard let story: Story = await WebService().downloadData(fromURL: url) else {
+        return nil
+    }
+    print("Story: \(story)")
+    return story
 }
